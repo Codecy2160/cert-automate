7dbb1a105747a3242c4f1b38f4598ce9912e2865
Codecy2160 <kageroucsqc@gmail.com>
2023-06-11 23:46:19 +0800

    Update

--- cert-automate.js
+++ cert-automate.js
@@ -1,7 +1,33 @@
-var files;
-var names;
+var files = File.openDialog("Select Photoshop files", "*.psd", false);
+var nameFile = File.openDialog("Select Text File with Names", "*.psd", false);
+var names = [ ];
 
-function main(x) {
-    var file = files[x];
-    
+if(!files) alert("No Photoshop file selected!");
+else if(!nameFile) alert("No Text file selected!");
+if(!files || !nameFile) $.gc;
+
+function createCerts(x) {
+    if(!names[x]) return;
+    var file = new File(files[0]);
+    var doc = app.open(file);
+    var nameLayer;
+    var textSize = 0;
+    for(var i = 0; i < doc.layers.length; i++) {
+        var layer = doc.layers[i];
+        if(layer.kind === LayerKind.TEXT) {
+            var textItem = layer.textItem;
+            if(textItem.content.length === 0 &&
+                textItem.size > textSize) nameLayer = layer;
+        }
+    }
+
+}
+
+function processNames() {
+    var file = new File(nameFile);
+    var name = file.open('r');
+    var contents = file.read();
+    file.close;
+    var delimiters = /\s\n\r,\|&/gmi;
+    names = contents.split(delimiters);
 }
